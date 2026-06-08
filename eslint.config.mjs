678491a5be4cb6ca28ff934c7c60d360de1f3643
ali <193356044+yawmagorped@users.noreply.github.com>
2026-06-08 14:03:56 +0330

    fixing jest and implementing Ship structure and its tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,11 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
-  eslintConfigPrettier,
-  {
-    node: true,
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+  { files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: {...globals.browser, ...globals.node, ...globals.jest} },
   },
 ]);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-import "./style.css";
+//import "./style.css";
+
+export { Ship };
+
+const Ship = (inputLength) => {
+  let _length = inputLength;
+  let _hitCount = 0;
+
+  const hit = (numberOfHits = 1) => {
+    _hitCount = _hitCount + numberOfHits;
+  }
+
+  const isSunk = () => {
+    if (_length == _hitCount) {
+      return true;
+    }
+    else
+      return false;
+  }
+
+  return {
+    get hitCount() {
+      return _hitCount;
+    },
+    get length() {
+      return _length;
+    },
+
+    hit, isSunk}
+};
